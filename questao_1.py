9fa4531beda7b3c65f5f2264af71900663212811
Sir Thales <tha10les@gmail.com>
2024-06-25 10:30:54 -0300

    correção de sintaxe

--- questao_1.py
+++ questao_1.py
@@ -1,6 +1,6 @@
 #O objetivo deste código é exercitar os conhecimentos a respeito da criação e manipulação de 
 #listas
-#A ideia do código é solicitar 5 inteiros, armazen=alos em uma lista e somar todos os inteiros
+#A ideia do código é solicitar 5 inteiros, armazena-los em uma lista e somar todos os inteiros
 #utilizando estruturas de repetição
 
 l1 = [0, 1, 2, 3, 4]
@@ -8,7 +8,7 @@ total = 0
 
 for i in range (0, 5):
     print('Digite o número de posição', i, ':')
-    l1[i] = int(input(''))
+    l1[i] = int(input('')
 
 print('Os inteiros na lista são:', l1)
 
